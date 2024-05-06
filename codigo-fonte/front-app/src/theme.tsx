5dc57d98c13563bdc8637738f4dbdcbd53b606b7
Lucas <lucolborges@hotmail.com>
2024-05-06 11:38:27 -0300

    bug layout jobcard

--- codigo-fonte/front-app/src/theme.tsx
+++ codigo-fonte/front-app/src/theme.tsx
@@ -11,6 +11,19 @@ const theme = createTheme({
         main: '#DFFF02'
       }
     },
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            boxShadow:  "0 10px 15px -1px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)",
+            transition: 'box-shadow 0.3s ease-in-out',
+            '&:hover': {
+              boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.15), 0 10px 10px -5px rgba(0, 0, 0, 0.1)',
+            }
+          }
+        }
+      }
+    }
   });
 
 
