61cb945a494dcd349b36e2e4bc2afd399aecf01b
LeoXavier13 <leonardo.xavier.1993@gmail.com>
2024-05-11 22:03:51 -0300

    subindo landingpage

--- codigo-fonte/front-app/src/pages/LandingPage.tsx
+++ codigo-fonte/front-app/src/pages/LandingPage.tsx
@@ -1,88 +1,65 @@
-import React, { useEffect,useState } from 'react';
-import PageContainerComponent from '../component/PageContainer';
-import type { AppDispatch, RootState } from '../reducers/store'
-import { useSelector, useDispatch } from 'react-redux'
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import { setDialog, setDialogIdle } from '../reducers/dialogReducer'
-import { title } from 'process';
-import DialogComponent from  '../component/Dialog'
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { Link, Navigate } from 'react-router-dom';
-import { post } from '../services/agent'
-import { Select, MenuItem, InputLabel, FormControl } from '@mui/material';
-import { authorizeUser } from '../reducers/userReducer'
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { Container, CssBaseline, Box, Stack, styled } from "@mui/material";
+
+const TypographyMold = styled(Typography)({
+  fontFamily: "red-hat-display",
+});
+
+const ValuesBox = ({ title, text }: { title: string; text: string }) => {
+  return (
+    <Box
+      display={"flex"}
+      gap={1}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      padding={2}
+      width={"400px"}
+      height={"200px"}
+      bgcolor={"white"}
+      borderRadius={5}
+      boxShadow={
+        "0 10px 15px -1px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)"
+      }
+    >
+      <TypographyMold fontSize={"20px"} fontWeight={600}>
+        {title}
+      </TypographyMold>
+      <TypographyMold fontSize={"20px"}>{text}</TypographyMold>
+    </Box>
+  );
+};
 
 const LandingPage = () => {
-  const isDialogOpen = useSelector((state: RootState) => state.dialog.isOpen)
-  const dispatch = useDispatch<AppDispatch>();
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [tipoLogin, setTipoLogin] = useState(1);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(setDialog({
-      title: 'LandingPage',
-      action: null
-    }))
-  },[])
-  const handleSubmit = async () => {
-    const userData = {
-      Email: email,
-      Senha: senha,
-      TipoLogin: tipoLogin  
-    };
-  
-    try {
-    const response = await post("Auth/login",userData);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); 
-        dispatch(authorizeUser({
-          token: data.token,
-          username: email,
-          name: email,
-          tipoLogin
-        }))
-        navigate('/')
-      } else {
-        alert('Falha no login');
-      }
-    } catch (error) {
-      console.error("Erro no login:", error);
-    }
-  };
   return (
-    <PageContainerComponent 
-      title="" 
-      style={{ 
-        marginLeft: isMobile ? 60 : 3000, 
-      }}>
-      <DialogComponent open={isDialogOpen} handleClose={() => dispatch(setDialogIdle())} >
-        {/*  mexer só daqui pra baixo */}
+    <React.Fragment>
+      <CssBaseline />
 
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography>Endereço de e-mail</Typography>
-            
-          </Grid>
-          
-                   
-        </Grid>
+      <Container maxWidth={"xl"}>
+        <Stack direction={"column"} spacing={10}>
+          <ValuesBox
+            title="  Missão"
+            text="     Um hub de negócios onde a clínica pode encontrar o profissional
+          que deseja e o profissional pode encontrar a vaga que tanto
+          procura."
+          />
 
+          <ValuesBox
+            title="Visão"
+            text=" A EcoVet é o ecossistema que pensa em conectar especialistas nas
+          diversas áreas da veterinária ao seu pet, oferecendo agilidade e
+          qualidade no atendimento."
+          />
 
-        {/*  mexer só daqui pra cima */}
-      </DialogComponent>
+          <ValuesBox
+            title="  Valores"
+            text="Transparência e solução de negócios de clínicas e profissionais
+          autônomos, democratizando o ambiente da medicina veterinária."
+          />
+        </Stack>
+      </Container>
+    </React.Fragment>
+  );
+};
 
-    </PageContainerComponent>
-  )
-}
-
-export default LandingPage
+export default LandingPage;
