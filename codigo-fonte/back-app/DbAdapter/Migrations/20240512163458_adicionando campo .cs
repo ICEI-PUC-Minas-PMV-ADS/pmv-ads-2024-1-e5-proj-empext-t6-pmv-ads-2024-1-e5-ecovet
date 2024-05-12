cfff6b5e43024f7a73b16079aef86a8bc3d465c9
Fellipe Pereira <fellipe.pereira@supermix.com.br>
2024-05-12 14:52:05 -0300

    criando vaga

---
+++ codigo-fonte/back-app/DbAdapter/Migrations/20240512163458_adicionando campo .cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbAdapter.Migrations
+{
+    public partial class adicionandocampo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<string>(
+                name: "TituloVaga",
+                table: "Vagas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClinicaVeterinariaIDClinica",
+                table: "Vagas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
