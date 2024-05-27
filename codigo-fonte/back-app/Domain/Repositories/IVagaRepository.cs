6bfb9b278ccee8a8f6e58d879ff369a9771c3855
Fellipe Pastor <fellipepastorp@gmail.com>
2024-05-27 19:18:00 -0300

    Obter vagas com id da clinica

--- codigo-fonte/back-app/Domain/Repositories/IVagaRepository.cs
+++ codigo-fonte/back-app/Domain/Repositories/IVagaRepository.cs
@@ -23,7 +23,7 @@ namespace Domain.Repositories
         /// <param>
         /// Email clinica
         /// </param>
-        Task<IEnumerable<Vaga>> ObterVagasClinicaAsync(int idClinica);
+        Task<IEnumerable<ObterVagaComClinica>> ObterVagasClinicaAsync(int idClinica);
 
         /// <summary>
         /// Deletar vaga.
