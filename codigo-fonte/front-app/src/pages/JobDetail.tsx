0789193f6c3305a05e0691cf2df427ed310d2e41
Lucas <lucolborges@hotmail.com>
2024-06-05 16:58:24 -0300

    layout bug

--- codigo-fonte/front-app/src/pages/JobDetail.tsx
+++ codigo-fonte/front-app/src/pages/JobDetail.tsx
@@ -5,35 +5,59 @@ import {
   CircularProgress,
   Container,
   CssBaseline,
+  Divider,
   Grid,
   Typography,
   styled,
   useMediaQuery,
 } from "@mui/material";
 import { useParams } from "react-router-dom";
-import { fakeData } from "../data";
+import JobCard from "../component/JobCard";
 
+// DELETAR FAKE DATA
 const fakeClinicWithHisJob = {
-  id: 1,
-  name: "John Dove",
-  location: "California",
-  email: "email@example.com",
-  contact: "99 9999 9999",
+  id: "2",
+  title: "Enfermeiro para cirurgia",
+  location: "Belo Horizonte MG",
+  value: "a combinar",
+  type: "urgent",
+  experience: "2 years",
   description:
     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
-  job: {
-    id: "2",
-    title: "Enfermeiro para cirurgia",
-    location: "Belo Horizonte MG",
-    value: "a combinar",
-    type: "urgent",
-    experience: "2 years",
+  responsibilities:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+  clinic: {
+    id: 1,
+    name: "John Dove",
+    location: "California",
+    email: "email@example.com",
+    contact: "99 9999 9999",
     description:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
-    responsibilities:
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
   },
 };
+
+const fakeJobCardData = [
+  {
+    id: "2",
+    tituloVaga: "Enfermeiro para cirurgia",
+    nome: "Clinica Ecovet",
+  location: "Belo Horizonte MG",
+  value: "a combinar",
+  type: "urgent",
+  experience: "2 years",
+  description:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+  responsibilities:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+  
+    clinicaVaga: {
+      
+    }
+  }
+]
+
+///////////////////////
 
 const TypographyMold = styled(Typography)({
   fontFamily: "red-hat-display",
@@ -101,9 +125,9 @@ const JobDetail = () => {
 
     const fetchSimilarJobs = async () => {
       try {
-        const jobsData = fakeData.slice(0, 6);
-        setSimilarJobs(jobsData);
-        setLoadingSimilarJobs(false);
+        // const jobsData = fakeData.slice(0, 6);
+        // setSimilarJobs(jobsData);
+        // setLoadingSimilarJobs(false);
       } catch (error) {
         console.log(error);
         setLoadingSimilarJobs(false);
@@ -178,10 +202,10 @@ const JobDetail = () => {
                         <TypographyMold
                           sx={{ fontSize: "20px", fontWeight: 600 }}
                         >
-                          {jobDetail.job.title}
+                          {jobDetail.title}
                         </TypographyMold>
                         <TypographyMold sx={{ fontSize: "16px" }}>
-                          {jobDetail.job.location}
+                          {jobDetail.location}
                         </TypographyMold>
                         <TypographyMold
                           sx={{ fontSize: "16px", color: "#2563eb" }}
@@ -191,7 +215,7 @@ const JobDetail = () => {
                         <TypographyMold
                           sx={{ fontSize: "14px", color: "#6b7280" }}
                         >
-                          {jobDetail.job.data}
+                          {jobDetail.data}
                         </TypographyMold>
                       </Box>
                     </Box>
@@ -241,27 +265,133 @@ const JobDetail = () => {
                   </Box>
 
                   <Box display={"flex"} gap={"16px"} paddingY={"20px"}>
-              <CssButton
-                onClick={() => setSelected("0")}
-                variant="contained"
-                fullWidth
-                sx={selected === "1" ? buttonStyle2 : buttonStyle1}
-              >
-                Descrição da vaga
-              </CssButton>
+                    <CssButton
+                      onClick={() => setSelected("0")}
+                      variant="contained"
+                      fullWidth
+                      sx={selected === "1" ? buttonStyle2 : buttonStyle1}
+                    >
+                      Descrição da vaga
+                    </CssButton>
 
-              <CssButton
-                onClick={() => setSelected("1")}
-                variant="contained"
-                fullWidth
-                sx={selected === "1" ? buttonStyle1 : buttonStyle2}
-              >
-                Clínica
-              </CssButton>
-            </Box>
+                    <CssButton
+                      onClick={() => setSelected("1")}
+                      variant="contained"
+                      fullWidth
+                      sx={selected === "1" ? buttonStyle1 : buttonStyle2}
+                    >
+                      Clínica
+                    </CssButton>
+                  </Box>
+
+                  <Box marginY={"24px"}>
+                    {selected === "0" ? (
+                      <>
+                        <Box>
+                          <TypographyMold fontSize={"20px"} fontWeight={600}>
+                            Descrição da Vaga
+                          </TypographyMold>
+                          <TypographyMold
+                            fontSize={"16px"}
+                            textAlign={"justify"}
+                          >
+                            {jobDetail.description}
+                          </TypographyMold>
+                        </Box>
+
+                        <Box>
+                          <TypographyMold
+                            fontSize={"20px"}
+                            fontWeight={600}
+                            marginTop={"32px"}
+                          >
+                            Requerimentos
+                          </TypographyMold>
+                          <TypographyMold
+                            textAlign={"justify"}
+                            fontSize={"16px"}
+                          >
+                            {jobDetail.responsibilities}
+                          </TypographyMold>
+                        </Box>
+                      </>
+                    ) : (
+                      <>
+                        <Box
+                          marginBottom={"24px"}
+                          display={"flex"}
+                          flexDirection={"column"}
+                        >
+                          <TypographyMold fontSize={"16px"}>
+                            {jobDetail.clinic.name}
+                          </TypographyMold>
+                          <TypographyMold>
+                            {jobDetail.clinic.location}
+                          </TypographyMold>
+                          <TypographyMold>
+                            {jobDetail.clinic.email}
+                          </TypographyMold>
+                        </Box>
+
+                        <TypographyMold fontSize={"20px"} fontWeight={600}>
+                          Sobre a clínica
+                        </TypographyMold>
+                        <TypographyMold>
+                          {jobDetail.clinic.description}
+                        </TypographyMold>
+                      </>
+                    )}
+                  </Box>
+                  <Divider />
                 </>
               )}
             </Grid>
+
+
+
+          <Grid
+            padding={"20px"}
+            sx={{
+              marginTop: { xs: "80px", sm: "0px" },
+            }}
+            item
+            sm={12}
+            md={6}
+          >
+              <TypographyMold
+              sx={{
+                color: "#6b7280",
+                fontWeight: 600,
+                opacity: 0.7,
+              }}
+            >
+              Vagas similares
+            </TypographyMold>
+
+            {loadingSimilarJobs ? (
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "20px",
+                  height: "100%",
+                }}
+              >
+                <CircularProgress />
+              </Box>
+            ) : similarJobs && similarJobs.length ? (
+              <>
+                <Box display={"flex"} flexWrap={"wrap"} gap={"16px"}>
+                  {/* {similarJobs.map((item) => (
+                    <JobCard job={item} key={item.id} />
+                  ))} */}
+                </Box>
+              </>
+            ) : (
+              <TypographyMold>Ainda não foram criadas vagas 😓 </TypographyMold>
+            )}
+          </Grid>
           </Grid>
         </Container>
       </React.Fragment>
