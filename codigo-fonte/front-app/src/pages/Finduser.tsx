7c6ee60fa789f991b6ae384df8aebb3526471564
ana carolina mendes <carolinamendescontato@gmail.com>
2024-06-05 17:10:18 -0300

    container

--- codigo-fonte/front-app/src/pages/Finduser.tsx
+++ codigo-fonte/front-app/src/pages/Finduser.tsx
@@ -1,8 +1,15 @@
+import { Container, CssBaseline } from "@mui/material";
 import React  from "react";
 
 const Finduser = () => {
     return (  
-        <></>
+        <React.Fragment> 
+            <CssBaseline/>
+            <Container maxWidth={"xl"}>
+
+            </Container>
+        </React.Fragment>
+
     )
 }
 export default Finduser
