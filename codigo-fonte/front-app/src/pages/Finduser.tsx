2f62112215a8737c91ab611878a2aa9abea5f603
Lucas <lucolborges@hotmail.com>
2024-06-06 21:48:37 -0300

    fake data

--- codigo-fonte/front-app/src/pages/Finduser.tsx
+++ codigo-fonte/front-app/src/pages/Finduser.tsx
@@ -19,6 +19,7 @@ import React, { KeyboardEvent, useEffect, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import UserBox from "../component/UserBox";
 
+// FAKE DATA DELETAR
 const usersData: any = [
   {
     id: 1,
@@ -143,6 +144,9 @@ const usersData: any = [
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
   },
 ];
+
+/////
+
 const TypographyModel = styled(Typography)({
   fontFamily: "red-hat-display",
 });
